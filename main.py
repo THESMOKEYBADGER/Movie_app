9b0cb59dfceb0968f214bbc6e6814185f608ba0c
THESMOKEYBADGER <gabrielrobbertze@gmail.com>
2023-04-23 09:51:17 +0200

    Adding menu and formatting

--- main.py
+++ main.py
@@ -1,55 +1,83 @@
-import requests
-import json
-import re
+import os
+import curses
+import time
 import pulls as pull
+import menu as menu
 
 
 def movie_enter():
 
-    movie_input = input("Enter the title of the movie you'd like to see: ")
+    movie_input = input("\n" + "Enter the title of the movie you'd like to see: ")
     return movie_input
 
 
 def choose_movie(mylist):
-       
+
     this_list = mylist[:15]
 
     if len(mylist) <= 10:
         print(f"Here are the {len(mylist)} movies:\n")
-    
+
     elif len(mylist) >= 15:
         print("Here are the top 15 results:\n")
 
     max_name_len = max(len(movie['title']) for movie in this_list)
-    
+
     for movie in this_list:
         year = movie['release_date'][:4] if movie['release_date'] else 'Unknown'
         print(f"-> {movie['title']:<{max_name_len}} {year}")
 
-
     if len(mylist) >= 1:
         print("\n\033[32mResults found!\033[0m\n")
+
+
+    print("Would you like to re-enter the title for a different set of results?")
+    print("Y for yes, or any other key for no...")
+
+    choice = (input().upper())
+
+    if choice == "Y":
+
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+        
+        movie_enter()
+        choose_movie(mylist)
+
+    print("\033[F\033[K", end="")
+    print("\033[F\033[K", end="")
+    print("\033[F\033[K", end="")
+
 
     while True:
         try:
             num_pick = int(
                 input("Enter the number associated with the movie: "))
+            if num_pick > len(mylist):
+                print(
+                    "\033[31mPlease enter a valid number in the range provided.\033[0m")
+                continue
             break
         except ValueError:
             print("Please enter a valid numerical character in the range provided:  ")
+
+    print(f"\n\033[32m{mylist[num_pick]['title']}\033[0m\n")
 
     return num_pick
 
 
 def print_actors(mylist, num_pick):
 
+    print("\n" + f"Here are the top acotors in \033[32m{mylist[num_pick]['title']}\033[0m:\n")
+
     actors = pull.get_movie_cast(mylist, num_pick)
 
-    num = 1
+    
 
     for actor in actors:
-        print(num, ": ", actor)
-        num += 1
+        print("- ", actor)
 
 
 def print_movie_details(mylist, num_pick):
@@ -61,17 +89,16 @@ def print_movie_details(mylist, num_pick):
     release_date = data['release_date']
     run_time = data['runtime']
     production_companies = [company['name']
-        for company in data['production_companies']]
-    
+                            for company in data['production_companies']]
+
     revenue = format(int(data['revenue']), ',')
 
-    print("Title:", title)
+    print("\n" + "Title:", f"\033[32m{title}\033[0m")
     print("Overview:", overview)
     print("Release date:", release_date,)
     print("Run time:", run_time,  " minutes")
     print("Production companies:", ", ".join(production_companies))
-    print("Revenue: $", revenue)
-
+    print("Revenue: $", revenue + "/n")
 
 
 def __main__():
@@ -84,12 +111,12 @@ def __main__():
 
     num_pick = choose_movie(mylist)
 
-    print_movie_details(mylist, num_pick)
+    menu.menu.print_menu(mylist, num_pick)
 
-    print_actors(mylist, num_pick)
-    
-    
-    
+    # print_movie_details(mylist, num_pick)
+
+    # print_actors(mylist, num_pick)
+
 
 if __name__ == "__main__":
     __main__()
---
+++ menu.py
@@ -0,0 +1,43 @@
+import main as main
+
+class menu:
+
+    def print_menu(mylist,num_pick):
+
+        while True:
+
+            # Display the menu options
+            print("Menu:")
+            print("1. See movie Detials")
+            print("2. See Cast")
+            print("3. Option 3")
+            print("4. Quit" + "\n")
+
+            choice = input("Enter your choice: " + "\n")
+
+            if choice == '1': 
+                print("\n" "Option 1 selected." "\n" )
+                main.print_movie_details(mylist,num_pick)
+                
+
+            elif choice == "2":
+                print("\n" "Option 2 selected." "\n" )
+                main.print_actors(mylist,num_pick)
+                
+
+            elif choice == "3":
+                # Do something for option 3
+                return
+                
+            
+            elif choice == "4":
+                # Quit the program
+                print("Quitting...")
+                break
+                
+            else:
+                # Handle invalid input
+                print("Invalid choice. Please try again.")
+        
+
+
